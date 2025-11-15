01f6c85c51d56335fc14420b4c5a0bc442ae6677
unknown <nagit commit -m first>
2025-11-15 17:11:47 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/cat-swipe-app/",
+  base: '/cat-swipe-app/',
   plugins: [react()],
 })
